96ff140348eea6407830b759cec2ad5796ae2be9
Stephen Ndele <stephenndele09346@gmail.com>
2021-03-25 22:13:49 +0300

    add first model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    name = models.CharField(max_length=250)
+    director = models.CharField(max_length=100)
+    cast = models.CharField(max_length=1000)
+    description = models.TextField(max_length=6000)
+    release_date = models.DateField()
+    averageRating = models.FloatField()
+
+    def __unicode__(self):
+        return self.name
+
+
+    def __str__(self):
+        return self.name
