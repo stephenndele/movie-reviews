3f07a292faaf8ee34522a2b04bb9e41151a80379
Stephen Ndele <stephenndele09346@gmail.com>
2021-03-27 10:34:50 +0300

    add edit reviews urls

--- main/urls.py
+++ main/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('editmovies/<int:id>/', views.edit_movies, name='edit_movies'),
     path('deletemovies/<int:id>/', views.delete_movies, name='delete_movies'),
     path('addreviews/<int:id>/', views.add_review, name='add_review'),
+    path('editreview/<int:movie_id>/<int:review_id>/', views.edit_review, name='edit_review'),
 ]
 
