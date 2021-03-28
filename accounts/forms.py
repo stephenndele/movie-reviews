ccf9433361e57384249dbac3237db05e6ca8a7ed
Stephen Ndele <stephenndele09346@gmail.com>
2021-03-28 20:00:09 +0300

    add welcome message

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,3 +6,8 @@ class RegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1','password2')
+
+# class NewsLetterForm(forms.Form):
+#     class Meta:
+#         model = User
+#         fields = ('email', 'username')
