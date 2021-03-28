fb01da173f5dfbff8eb902adf66e59f041ae06a8
Stephen Ndele <stephenndele09346@gmail.com>
2021-03-28 20:50:51 +0300

    deploy to heroku

--- moviereview/settings.py
+++ moviereview/settings.py
@@ -25,7 +25,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u-vwfb$=126%if%f_ww*^t-14$+s)x4!m-zd6$nonb%f_xwtt6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
