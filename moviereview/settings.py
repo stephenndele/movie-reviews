e88d1ee43536d6f1cb3e816848cbab5efc886ab0
Stephen Ndele <stephenndele09346@gmail.com>
2021-03-29 08:59:10 +0300

    add middle ware

--- moviereview/settings.py
+++ moviereview/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
 ]
 
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
