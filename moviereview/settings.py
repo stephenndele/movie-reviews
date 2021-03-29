8170610770a59700d29ed44044fb23815056739a
Stephen Ndele <stephenndele09346@gmail.com>
2021-03-29 13:54:00 +0300

    add static folder

--- moviereview/settings.py
+++ moviereview/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = []
 
 
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'moviereview.wsgi.application'
 
 MODE=config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
+# DEBUG = config('DEBUG', default=False, cast=bool)
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
