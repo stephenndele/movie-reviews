fd75be226ed3ee03c109b8c2231d1ba52cbacb7f
Stephen Ndele <stephenndele09346@gmail.com>
2021-03-29 17:49:57 +0300

    add placeholder for image

--- main/forms.py
+++ main/forms.py
@@ -6,6 +6,10 @@ class MovieForm(forms.ModelForm):
         model = Movie
         fields = ('name', 'director', 'cast', 'description', 'release_date', 'image')
 
+        widgets = {
+            'image': forms.TextInput(attrs={'placeholder': 'Add image url.... '}),
+        }
+
 
 class ReviewForm(forms.ModelForm):
     class Meta:
