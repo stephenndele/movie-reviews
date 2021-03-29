e30829f9caf4245709759b50e3b2677786e3b8a7
Stephen Ndele <stephenndele09346@gmail.com>
2021-03-29 08:47:23 +0300

    add content

--- moviereview/settings.py
+++ moviereview/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'moviereview.wsgi.application'
 
 MODE=config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
